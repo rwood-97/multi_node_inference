5d39107d18120626de5fc11d9baf41baff67106f
Rosie Wood <rwood@turing.ac.uk>
2025-11-21 12:02:05 +0000

    update HF_HOME and PIP_CACHE_DIR for training

--- transformers_examples/baskerville/run_local.sh
+++ transformers_examples/baskerville/run_local.sh
@@ -2,8 +2,9 @@ module purge
 module load baskerville
 module load Python CUDA
 
-export PIP_CACHE_DIR=$(PWD -P)/.cache/pip
-export HF_HOME=$(PWD -P)/.cache/huggingface
+# for hpc training, set cache dirs as part of project folder
+export PIP_CACHE_DIR=/bask/projects/v/vjgo8416-hpc2511/.cache/pip
+export HF_HOME=/bask/projects/v/vjgo8416-hpc2511/.cache/huggingface
 
 python -m venv venv_a100
 source venv_a100/bin/activate
